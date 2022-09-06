33fb8896826e1e424cba2ffcf7f5d10e9c9dcff1
drake918 <drakeaxel.mayo@benilde.edu.ph>
2022-09-06 19:11:01 +0800

    Added Game File, and Initial Gameplay Prototype

---
+++ Shoot-Out Showdown Game/Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Serves as Top Down Player Movement
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    
+    private Rigidbody2D rb;
+    private float currentSpeed;
+    private Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentSpeed = Speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Move Function
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        rb.velocity = moveInput * currentSpeed;
+    }
+}
---
+++ Shoot-Out Showdown Game/Assets/Shooting.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Executes Shooting Mechanic
+public class Shooting : MonoBehaviour
+{
+    public Camera Cam;
+    
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    private Vector2 aimInput;
+    private PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Aim with Mouse
+    public void MouseAim(InputAction.CallbackContext context)
+    {
+        if (playerInput.defaultControlScheme == "Keyboard&Mouse")
+        {
+            aimInput = Cam.ScreenToWorldPoint(context.ReadValue<Vector2>());
+
+            direction = aimInput - rb.position;
+            SetAngle();
+        }
+    }
+
+    // Aim with Right Joystick
+    public void ControllerAim(InputAction.CallbackContext context)
+    {
+        if (playerInput.defaultControlScheme == "Controller")
+        {
+            aimInput = context.ReadValue<Vector2>();
+
+            // Maintain Angle
+            // Prevents Snapping back to Zero Angle when Releasing Joystick
+            if (aimInput.x != 0f && aimInput.y != 0f)
+            {
+                direction = aimInput;
+                SetAngle();
+            }
+        }
+    }
+
+    // Shoot Out a Bullet
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        Debug.Log("Shoot");
+    }
+
+    // Set Player Rotation
+    public void SetAngle()
+    {
+        float rotate = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90f;
+
+        rb.MoveRotation(rotate);
+    }
+}
