9502efc5038409db4db8d861f24e5306b17c30e6
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-01-03 00:42:57 +0800

    Improved BulletCatcher Script

--- Shoot-Out Showdown Game/Assets/Scripts/BulletCatcher.cs
+++ Shoot-Out Showdown Game/Assets/Scripts/BulletCatcher.cs
@@ -7,9 +7,6 @@ public class BulletCatcher : MonoBehaviour
     void OnTriggerExit2D(Collider2D collider)
     {
         if (collider.CompareTag("Bullet"))
-        {
-            Debug.Log(collider.gameObject.name + " caught!");
             collider.GetComponent<Poolable>().ReturnToPool();
-        }
     }
 }
