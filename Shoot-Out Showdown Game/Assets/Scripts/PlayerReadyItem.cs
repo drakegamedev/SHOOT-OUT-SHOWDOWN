1f900bd3125bd81ddec9dc12a02056f74c8f7ab6
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-04-16 19:30:53 +0800

    Improved Naming Conventions for PlayerReadyItem Script

--- Shoot-Out Showdown Game/Assets/Scripts/PlayerReadyItem.cs
+++ Shoot-Out Showdown Game/Assets/Scripts/PlayerReadyItem.cs
@@ -1,28 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class PlayerReadyItem : MonoBehaviour
 {
-    public Color ReadyColor;
-    public TextMeshProUGUI PlayerText;
-    public TextMeshProUGUI ReadyText;
+    [Header("Properties")]
+    [SerializeField] private Color readyColor;                      // Ready Color Indicator
+
+    [Header("References")]
+    [SerializeField] private TextMeshProUGUI playerText;            // Player Text Reference
+    [SerializeField] private TextMeshProUGUI readyText;             // Ready Text Reference
 
     // Private Variables
-    private Outline outline;
+    private Outline outline;                                        // Outline Component Reference
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         outline = GetComponent<Outline>();
     }
 
+    /// <summary>
+    /// Indicate this Player as Ready
+    /// </summary>
     public void Ready()
     {
-        outline.effectColor = ReadyColor;
-        PlayerText.color = ReadyColor;
-        ReadyText.color = ReadyColor;
+        outline.effectColor = readyColor;
+        playerText.color = readyColor;
+        readyText.color = readyColor;
     }
 }
