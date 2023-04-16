b165d8f2e9ef2f10f5935f71899d974b040c53ce
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-04-16 18:57:13 +0800

    Added Comments to Camera Controller Script and Improved Bullet Catcher Script

--- Shoot-Out Showdown Game/Assets/Scripts/BulletCatcher.cs
+++ Shoot-Out Showdown Game/Assets/Scripts/BulletCatcher.cs
@@ -1,10 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletCatcher : MonoBehaviour
 {
-    void OnTriggerExit2D(Collider2D collider)
+    private void OnTriggerExit2D(Collider2D collider)
     {
         if (collider.CompareTag("Bullet"))
             collider.GetComponent<Poolable>().ReturnToPool();
--- Shoot-Out Showdown Game/Assets/Scripts/CameraController.cs
+++ Shoot-Out Showdown Game/Assets/Scripts/CameraController.cs
@@ -4,18 +4,17 @@ public class CameraController : MonoBehaviour
 {
     private Animator animator;                                                  // Animator Component Reference
 
-    #region Initialization Functions
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
     }
-    #endregion
 
-    #region Public Functions
+    /// <summary>
+    /// Provides a Shaking Effect to the Camera
+    /// </summary>
     public void CameraShake()
     {
         animator.SetTrigger("shake");
     }
-    #endregion
 }
