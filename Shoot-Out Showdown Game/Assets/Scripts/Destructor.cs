ab2ec5ea98c9e4eb1ef43e848dee760f311074f8
Axel918 <drakeaxel.mayo@benilde.edu.ph>
2023-04-16 19:14:28 +0800

    Improved Naming Convention for Destructor Script

--- Shoot-Out Showdown Game/Assets/Scripts/Destructor.cs
+++ Shoot-Out Showdown Game/Assets/Scripts/Destructor.cs
@@ -4,35 +4,30 @@ using UnityEngine;
 // Object Destructor
 public class Destructor : MonoBehaviour
 {
-    public float DestructorTime;                                            // Destructor Timer Count
+    [Header("Properties")]
+    [SerializeField] private float destructorTime;                          // Destructor Timer Count
     
     // Private Variables
     private Poolable poolable;                                              // Poolable Class Reference
 
-    #region Enable/Disable Functions
-    void OnEnable()
+    private void OnEnable()
     {
         StartCoroutine(Destruct());
     }
-    #endregion
 
-    #region Initialization Functions
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Destruct());
-    }
-    #endregion
-
-    #region Coroutines
-    // Initiate Destructor Timer
-    IEnumerator Destruct()
-    {
         poolable = GetComponent<Poolable>();
+    }
 
-        yield return new WaitForSeconds(DestructorTime);
-
+    /// <summary>
+    /// Initiate Destructor Timer
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Destruct()
+    {
+        yield return new WaitForSeconds(destructorTime);
         poolable.ReturnToPool();
     }
-    #endregion
 }
